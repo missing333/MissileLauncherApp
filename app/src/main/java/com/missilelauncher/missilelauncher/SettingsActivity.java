8e218c7cd4017c31d25b8605ec7a417ec27bd5f1
Marc <mmissing3@gmail.com>
2025-12-30 16:43:16 -0700

    shifting position of menu elements so they are clickable.

--- app/src/main/java/com/missilelauncher/missilelauncher/SettingsActivity.java
+++ app/src/main/java/com/missilelauncher/missilelauncher/SettingsActivity.java
@@ -11,31 +11,39 @@ import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
-import android.preference.PreferenceActivity;
 import androidx.core.content.ContextCompat;
 import androidx.appcompat.app.ActionBar;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.List;
 
 import static android.preference.Preference.*;
 
-/**
- * A {@link PreferenceActivity} that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- * <p>
- * See <a href="http://developer.android.com/design/patterns/settings.html">
- * Android Design: Settings</a> for design guidelines and the <a
- * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
- * API Guide</a> for more information on developing a Settings UI.
- */
+
 public class SettingsActivity extends AppCompatPreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //set padding  here::
+        // Get the internal ListView
+        ListView listView = getListView();
+
+        // Convert 100dp to pixels
+        int paddingPx = (int) (100 * getResources().getDisplayMetrics().density);
+
+        // Set padding (Left, Top, Right, Bottom)
+        listView.setPadding(0, paddingPx, 0, 0);
+
+        // Ensure the list items don't get cut off when scrolling
+        listView.setClipToPadding(false);
+    }
 
     /**
      * A preference value change listener that updates the preference's summary
